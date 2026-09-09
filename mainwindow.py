a1c8bc6793739af473e6caa260814cbd29376478
justenoughwrong <justenoughwrong@gmail.com>
2026-09-09 00:25:03 -0400

    Version 1.0.2

--- mainwindow.py
+++ mainwindow.py
@@ -15,9 +15,10 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QButtonGroup, QComboBox, QHBoxLayout,
-    QMainWindow, QMenuBar, QRadioButton, QSizePolicy,
-    QStatusBar, QTextBrowser, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QButtonGroup, QComboBox, QGridLayout,
+    QHBoxLayout, QMainWindow, QMenuBar, QRadioButton,
+    QSizePolicy, QStatusBar, QTextBrowser, QVBoxLayout,
+    QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -27,15 +28,13 @@ class Ui_MainWindow(object):
         MainWindow.setAcceptDrops(False)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.layoutWidget = QWidget(self.centralwidget)
-        self.layoutWidget.setObjectName(u"layoutWidget")
-        self.layoutWidget.setGeometry(QRect(21, 18, 781, 901))
-        self.verticalLayout = QVBoxLayout(self.layoutWidget)
+        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.websiteRadioButton = QRadioButton(self.layoutWidget)
+        self.websiteRadioButton = QRadioButton(self.centralwidget)
         self.templateSelectionButtonGroup = QButtonGroup(MainWindow)
         self.templateSelectionButtonGroup.setObjectName(u"templateSelectionButtonGroup")
         self.templateSelectionButtonGroup.addButton(self.websiteRadioButton)
@@ -45,7 +44,7 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout.addWidget(self.websiteRadioButton, 0, Qt.AlignmentFlag.AlignRight)
 
-        self.gunbrokerRadioButton = QRadioButton(self.layoutWidget)
+        self.gunbrokerRadioButton = QRadioButton(self.centralwidget)
         self.templateSelectionButtonGroup.addButton(self.gunbrokerRadioButton)
         self.gunbrokerRadioButton.setObjectName(u"gunbrokerRadioButton")
         self.gunbrokerRadioButton.setEnabled(True)
@@ -57,7 +56,7 @@ class Ui_MainWindow(object):
 
         self.verticalLayout.addLayout(self.horizontalLayout)
 
-        self.templateSelect = QComboBox(self.layoutWidget)
+        self.templateSelect = QComboBox(self.centralwidget)
         self.templateSelect.setObjectName(u"templateSelect")
         self.templateSelect.setEditable(False)
         self.templateSelect.setMaxVisibleItems(30)
@@ -66,11 +65,14 @@ class Ui_MainWindow(object):
 
         self.verticalLayout.addWidget(self.templateSelect)
 
-        self.textBrowser = QTextBrowser(self.layoutWidget)
+        self.textBrowser = QTextBrowser(self.centralwidget)
         self.textBrowser.setObjectName(u"textBrowser")
         self.textBrowser.setFocusPolicy(Qt.FocusPolicy.NoFocus)
 
         self.verticalLayout.addWidget(self.textBrowser)
+
+
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
